e21e9a8d74be0ab252a45c9dee912426e795a535
Nik91K <nikotakovsl@gmail.com>
2025-05-31 14:25:20 +0300

    connected the main page

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,8 @@ import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Route, Routes } from 'react-router'
 import RegisterPage from './pages/Authentication/RegisterPage/RegisterPage'
 import LoginPage from './pages/Authentication/LoginPage/loginPage.tsx'
+import MainPage from './pages/Main/MainPage.tsx'
+import PrivateRoute from './pages/Authentication/PrivateRoute/PrivateRoute.tsx'
 import { Provider } from 'react-redux'
 import { store } from './api/store.ts'
 
@@ -15,6 +17,7 @@ createRoot(document.getElementById('root')!).render(
         <Route path='/' element={<App />} />
         <Route path='/register' element={<RegisterPage />} />
         <Route path='/login' element={<LoginPage />} />
+        <Route path='/sleeptracker' element={<PrivateRoute><MainPage /></PrivateRoute>} />
       </Routes>
     </BrowserRouter>
   </Provider>
