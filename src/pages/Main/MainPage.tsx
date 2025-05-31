a99cf7ced7f20c4918d6bd2d4d050d1eb27f7bce
Nik91K <nikotakovsl@gmail.com>
2025-05-31 14:24:15 +0300

    added sleep tracker home page

---
+++ src/pages/Main/MainPage.tsx
@@ -0,0 +1,14 @@
+import './styles.css';
+import LayoutPage from '../../layoutPage/layoutPage';
+
+const MainPage = () => {
+    return (
+        <LayoutPage title='Main'>
+            <div className="main-page">
+                <h1>Text</h1>
+            </div>
+        </LayoutPage>
+    )
+}
+
+export default MainPage
