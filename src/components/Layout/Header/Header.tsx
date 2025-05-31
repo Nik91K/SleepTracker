216b7f2157f910f8a2bc12e7fe56d74a9d5573ed
Nik91K <nikotakovsl@gmail.com>
2025-05-31 18:12:37 +0300

    Update header UI; hide sidebar for unauthenticated users

--- src/components/Layout/Header/Header.tsx
+++ src/components/Layout/Header/Header.tsx
@@ -1,12 +1,20 @@
 import { TfiAlarmClock } from "react-icons/tfi";
 import { MdOutlineAppRegistration } from "react-icons/md";
 import { IoLogInOutline } from "react-icons/io5";
-
+import Sitebar from "../Sitebar/Sitebar";``
 import './Header.css'
 
-const Header = () => {
+interface HeaderProps{
+    isLoggedIn: boolean;
+}
+
+const Header: React.FC<HeaderProps> = ({ isLoggedIn }) => {
+
     return (
         <header className="header">
+            {isLoggedIn && (
+                <Sitebar />
+            )}
             <div className="header-title">
                 <TfiAlarmClock size={35} />
                 <span className="header-title">Sleep tracker</span>
@@ -21,7 +29,9 @@ const Header = () => {
                 <a href="/register" className="link header-link">
                     <div>
                         <IoLogInOutline size={20}/>
+                        {!isLoggedIn && (
                         <span>Реєстрація</span>
+                        )}
                     </div>
                 </a>
             </div>
