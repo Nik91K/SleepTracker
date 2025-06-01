f1c71ba20ec3a3600bb9d5b03b1dc176dc7afaf6
Nik91K <nikotakovsl@gmail.com>
2025-06-01 21:33:01 +0300

    added a check for the header to see if you are logged in

--- src/layoutPage/layoutPage.tsx
+++ src/layoutPage/layoutPage.tsx
@@ -1,11 +1,19 @@
 import './layoutPage.css';
 import Header from '../components/Layout/Header/Header'
 import Footer from '../components/Layout/Footer/Footer'
+import { useEffect, useState } from 'react';
 
 const LayoutPage = ({title, children} : {title?:string, children:React.ReactNode}) => {
+    const [isLoggedIn, setIsLoggedIn] = useState(false);
+
+    useEffect(() => {
+        const token = localStorage.getItem('token');
+        setIsLoggedIn(!!token)
+    }, []);
+
     return (
         <div>
-            <Header />
+            <Header isLoggedIn={isLoggedIn} />
             <main className='main-container'>
                 <h2 className='layout-h2'>{title}</h2>
                 <div className="content">
