9fec002703f7bc04c86cc7c0786820ac1d69f2ff
Nik91K <nikotakovsl@gmail.com>
2025-06-05 00:58:37 +0300

    updated store.ts

--- src/api/store.ts
+++ src/api/store.ts
@@ -1,9 +1,11 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './slices/authSlice'
+import sleepRecordSlice from './slices/SleepRecord';
 
 export const store = configureStore({
     reducer: {
         auth: authSlice,
+        sleepRecord: sleepRecordSlice,
     },
 })
 
