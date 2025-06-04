7883709f35c0bf7bb7fe88b3f963b039c7622e66
Nik91K <nikotakovsl@gmail.com>
2025-06-05 00:58:15 +0300

    added sleep record slice

---
+++ src/api/slices/SleepRecord.ts
@@ -0,0 +1,52 @@
+import { createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from '../axios';
+import type { SleepRecordType } from "../../types/sleepRecordTypes";
+
+interface SleepRecordState {
+    records: SleepRecordType[];
+    loading: boolean;
+    error: string | null;
+    token: string | null;
+}
+
+const initialState: SleepRecordState = {
+    records: [],
+    loading: false,
+    error: null,
+    token: localStorage.getItem('token') || null,
+}
+
+export const createSleepRecord = createAsyncThunk (
+    'sleep-record',
+    async (recordData: { date: string, fellAsleepAt: string, wokeUpAt: string }, { rejectWithValue }) => {
+        try {
+            const response:any = await axios.post('/sleep-record', recordData)
+            return response.data
+        } catch (error: any) {
+            return rejectWithValue(error.response.data)
+        }
+    }
+)
+
+const sleepRecordSlice = createSlice({
+    name: "sleepRecord",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(createSleepRecord.pending, (state) => {
+            state.loading = true
+            state.error = null
+        })
+        .addCase(createSleepRecord.fulfilled, (state, action) => {
+            state.loading = false
+            state.records.push(action.payload)
+        })
+        .addCase(createSleepRecord.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.payload as string
+        })
+    }
+})
+
+export default sleepRecordSlice.reducer
