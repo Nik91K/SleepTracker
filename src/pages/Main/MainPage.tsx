aacc8ceca5ac2fe4e4d2bf3bf9d848b97350d0b7
Nik91K <nikotakovsl@gmail.com>
2025-06-06 18:56:28 +0300

    Improve handleSubmit logic and store form data in localStorage

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -6,6 +6,11 @@ import { useNavigate } from 'react-router';
 import { IoLogOutOutline } from "react-icons/io5";
 import SubmitButton from '../../components/common/Buttons/SubmitButton';
 import Input from '../../components/common/Inputs/Text';
+import { getFormInputValueByName } from '../../utils/getInput';
+import { useAppDispatch, useAppSelector } from '../../api/hooks';
+import { createSleepRecord } from '../../api/slices/SleepRecord';
+import Tooltip from '../../components/common/Tooltip';
+import React from 'react';
 
 const MainPage = () => {
     const send = useNavigate()
@@ -13,34 +18,78 @@ const MainPage = () => {
         localStorage.removeItem('token')
         send('/')
     }
-    const handleSubmit = (e: React.MouseEvent<HTMLButtonElement>) => {
-        e.preventDefault();
 
+    const dispatch = useAppDispatch()
+    const { loading, error } = useAppSelector(state => state.sleepRecord)
+    const [ tooltip, setError ] = React.useState<{ type: 'success' | 'error'; message: string } | null>(null)
+    const [ date, setDate ] = React.useState('')
+    const [fellAsleepAt, setFellAsleepAt] = React.useState('')
+    const [wokeUpAt, setWokeUpAt] = React.useState('')
+
+    React.useEffect(() => {
+        const savedData = JSON.parse(localStorage.getItem('sleepData') || '{}');
+        if (savedData.date) setDate(savedData.date);
+        if (savedData.fellAsleepAt) setFellAsleepAt(savedData.fellAsleepAt);
+        if (savedData.wokeUpAt) setWokeUpAt(savedData.wokeUpAt);
+    }, []);
+
+    const handleSubmit = async(event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault();
+        const date = getFormInputValueByName(event.currentTarget, 'date-track')
+        const fellAsleepAt = getFormInputValueByName(event.currentTarget, 'fellAsleepAt')
+        const wokeUpAt = getFormInputValueByName(event.currentTarget, 'wokeUpAt')
+
+        if (!date || !fellAsleepAt || !wokeUpAt) {
+            setError({type:'error', message:'Будь ласка, заповніть поля.'})
+            return
+        }
+
+        dispatch(createSleepRecord({ date, fellAsleepAt, wokeUpAt }))
+        .unwrap()
+        .then(() => {
+            setError({type: 'success', message: 'Дані додано'})
+            localStorage.setItem('sleepData', JSON.stringify({ date, fellAsleepAt, wokeUpAt }))
+        }) .catch((error) => {
+            setError({ type: 'error', message: error })
+        })
+        event.currentTarget.reset()
     }
+
+
+
 
     return (
         <LayoutPage>
+            {tooltip && (
+                <Tooltip type={tooltip.type} typeText={tooltip.message} close={() => setError(null)}/>
+            )} 
             <div className="main-page">
                 <Sitebar>
                     <ModalButton buttonName='Додати час'>
-                        <form className="modal-form">
+                        <form className="modal-form" onSubmit={handleSubmit}>
                             <h2>Заповніть дані</h2>
-
-                            <Input type="date" name="name" title='Дата' />
+                            {loading && <p>Завантаження</p>}
+                            <Input type="date" name="date-track" title='Дата' value={date} onChange={(e) => setDate(e.target.value)} />
                             <Input
                                 type="text"
                                 title='Коли ви лягли спаси'
                                 name="fellAsleepAt"
                                 pattern="^([01]\d|2[0-3]):([0-5]\d)$"
+                                value={fellAsleepAt}
+                                onChange={(e) => setFellAsleepAt(e.target.value)}
+
                             />
                             <Input
                                 type="text"
                                 title='Коли ви прокинулись'
-                                name="fellAsleepAt"
+                                name="wokeUpAt"
                                 pattern="^([01]\d|2[0-3]):([0-5]\d)$"
+                                value={wokeUpAt}
+                                onChange={(e) => setWokeUpAt(e.target.value)}
+
                             />
                             23:59
-                            <SubmitButton type='button' onClick={handleSubmit} text='Надіслати' />
+                            <SubmitButton type='submit' text='Надіслати' />
                         </form>
                     </ModalButton>
                     <div className='logout-button' onClick={logout}>
