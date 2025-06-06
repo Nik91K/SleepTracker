e059f8ccf3e005a666b1bdd694fe866e8c8fb0b5
Nik91K <nikotakovsl@gmail.com>
2025-06-06 18:54:29 +0300

    added onChange property

--- src/components/common/Inputs/Text/index.tsx
+++ src/components/common/Inputs/Text/index.tsx
@@ -1,6 +1,6 @@
 import './style.css'
 
-const Input = ({type, name, minLength, maxLength, id, title, value, pattern} : {type:string, name?:string, minLength?:number, maxLength?:number, id?: string, title: string, value?: string, pattern?: string}) => {
+const Input = ({type, name, minLength, maxLength, id, title, value, pattern, onChange} : {type:string, name?:string, minLength?:number, maxLength?:number, id?: string, title: string, value?: string, pattern?: string, onChange?: (e: React.ChangeEvent<HTMLInputElement>) => void}) => {
     return (
       <div className="input-container">
         <input 
@@ -13,6 +13,7 @@ const Input = ({type, name, minLength, maxLength, id, title, value, pattern} : {
           value={value}
           placeholder=""
           pattern={pattern}
+          onChange={onChange}
         />
         <span className="highlight"></span>
         <span className="bar"></span>
