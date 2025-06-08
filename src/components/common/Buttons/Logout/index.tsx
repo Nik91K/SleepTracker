58aebe82f92102aec92533fe699a5d42ab0b3f5e
Nik91K <nikotakovsl@gmail.com>
2025-06-08 22:22:41 +0300

    Move logout button to its own component

---
+++ src/components/common/Buttons/Logout/index.tsx
@@ -0,0 +1,22 @@
+import { IoLogOutOutline } from 'react-icons/io5'
+import './style.css'
+import { useNavigate } from 'react-router'
+
+const LogoutButton = () => {
+    const send = useNavigate()
+    const logout = () => {
+        localStorage.removeItem('token')
+        localStorage.removeItem('UserName')
+        localStorage.removeItem('UserEmail')
+        send('/')
+    }
+
+    return (
+        <div className='logout-button' onClick={logout}>
+           <IoLogOutOutline />
+            <p>Вийти з акканту</p>
+        </div>
+    )
+}
+
+export default LogoutButton
