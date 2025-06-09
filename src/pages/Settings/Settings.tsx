51321181f48aa037dd1d7b4d9251f6aa20bcac54
Nik91K <nikotakovsl@gmail.com>
2025-06-09 21:52:51 +0300

    Add settings component

---
+++ src/pages/Settings/Settings.tsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+const Settings = () => {
+    return (
+        <div className="settings-page">
+            <h1>Налаштування</h1>
+            <p>Ця сторінка знаходиться в розробці.</p>
+            <p>Будь ласка, поверніться пізніше.</p>
+        </div>
+    )
+}
+
+export default Settings
