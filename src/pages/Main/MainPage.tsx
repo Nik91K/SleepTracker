ce8b4b903f30853b27bcdb74b271e3aa6c0169b9
Nik91K <nikotakovsl@gmail.com>
2025-06-10 18:58:38 +0300

    update sleep track page

--- src/pages/Main/MainPage.tsx
+++ src/pages/Main/MainPage.tsx
@@ -1,18 +1,7 @@
 import './styles.css';
 import LayoutPage from '../../layoutPage/layoutPage';
-import Sitebar from '../../components/Layout/Sitebar/Sitebar';
-import ModalButton from '../../components/common/Modal/Open modal/index'
-import { Link, useLocation, useNavigate } from 'react-router';
-import { IoLogOutOutline } from "react-icons/io5";
-import SubmitButton from '../../components/common/Buttons/SubmitButton';
-import Input from '../../components/common/Input';
-import { getFormInputValueByName } from '../../utils/getInput';
-import { useAppDispatch, useAppSelector } from '../../api/hooks';
-import { createSleepRecord } from '../../api/slices/SleepRecord';
 import Tooltip from '../../components/common/Tooltip';
 import React, { useEffect } from 'react';
-import SleepRecordForm from '../../components/common/Forms/SleepRecord';
-import BasicPopover from '../../components/common/Popover';
 
 const MainPage = () => {
 
