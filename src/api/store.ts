90dea18064582cf214baf4676a70ca22cfe43ae3
Nik91K <nikotakovsl@gmail.com>
2025-06-14 16:49:42 +0300

    add sleep efficiency to store

--- src/api/store.ts
+++ src/api/store.ts
@@ -1,11 +1,13 @@
 import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './slices/authSlice'
 import sleepRecordSlice from './slices/SleepRecord';
+import sleepEfficiencySlice from './slices/sleepEfficiencySlice';
 
 export const store = configureStore({
     reducer: {
         auth: authSlice,
         sleepRecord: sleepRecordSlice,
+        sleepEfficiency: sleepEfficiencySlice,
     },
 })
 
