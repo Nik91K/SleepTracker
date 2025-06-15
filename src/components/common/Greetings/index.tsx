df3f90f6091a6f5d2d9cb01698b64507a6407331
Nik91K <nikotakovsl@gmail.com>
2025-06-15 16:39:13 +0300

    added useEffect so that the function is executed only once

--- src/components/common/Greetings/index.tsx
+++ src/components/common/Greetings/index.tsx
@@ -1,26 +1,33 @@
+import React from 'react';
 import './style.css';
 
 const Greetings = () => {
     const name = localStorage.getItem('UserName') || 'Користувач'
-    const randomGreetings = Math.random()
-    const hours = new Date().getHours()
-    let text = ''
+    const [greetingsText, setGreetingsText] = React.useState('')
 
-    if (randomGreetings < 0.4) {
-        text = 'Вітаємо'
-    } else if (hours >= 5 && hours < 12) {
-        text = 'Доброго ранку'
-    } else if (hours >= 12 && hours < 18) {
-        text = 'Доброго дня'
-    } else if (hours >= 18 && hours < 21) {
-        return 'Доброго вечора'
-    } else {
-        text = 'Доброї ночі'
-    }
+    React.useEffect(() => {
+        const randomGreetings = Math.random()
+        const hours = new Date().getHours()
+
+        if (randomGreetings < 0.4) {
+            setGreetingsText('Вітаємо')
+        } else if (hours >= 5 && hours < 12) {
+            setGreetingsText('Доброго ранку')
+        } else if (hours >= 12 && hours < 18) {
+            setGreetingsText('Доброго дня')
+        } else if (hours >= 18 && hours < 21) {
+            setGreetingsText('Доброго вечора')
+        } else {
+            setGreetingsText('Доброї ночі')
+        }
+    }, [])
+
+
+
     return (
         <div className="greetings">
             <p className='greetings-username'>{name}</p>
-            <p className="greetings-text">{text}!</p>
+            <p className="greetings-text">{greetingsText}!</p>
         </div>
     )
 }
