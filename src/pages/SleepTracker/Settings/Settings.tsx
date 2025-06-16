9b3e74c9781ebb534dd99f33ff782d9dff4746d0
Nik91K <nikotakovsl@gmail.com>
2025-06-16 20:18:06 +0300

    redesigned the settings page

---
+++ src/pages/SleepTracker/Settings/Settings.tsx
@@ -0,0 +1,40 @@
+import LayoutPage from '../../../layoutPage/layoutPage'
+import ReminderForm from '../../../components/common/Forms/Reminder'
+import DataInformation from '../../../components/Layout/SettingsBlocks/SettingsBlocks'
+import ModalButton from '../../../components/common/Modal/Open modal'
+import { FaUser, FaBell } from 'react-icons/fa'
+import { MdEmail } from 'react-icons/md'
+import { IoMdSettings } from "react-icons/io";
+import './style.css'
+
+const Settings = () => {
+    console.log("Settings page rendered")
+    const name = localStorage.getItem('name') || 'Користувач'
+    const email = localStorage.getItem('email') || 'email@example.com'
+
+    const personalInfo = [
+        { label: name, icon: <FaUser /> },
+        { label: email, icon: <MdEmail /> }
+    ]
+
+    const customization = [
+        { label: 'Тема', icon: <IoMdSettings /> },
+        { label: 'Сповіщення', value: <ModalButton buttonName='Керувати сповіщеннями'><ReminderForm /></ModalButton>, icon: <FaBell /> }
+    ]
+
+    return (
+        <LayoutPage>
+            <div className="sleep-tracker-header">
+                <p>Налаштування</p>
+            </div>
+            <div className="settings-page">
+                <DataInformation title='Персональна інформація' items={personalInfo} />
+                <DataInformation title='Кастомізація' items={customization} />
+                
+            </div>
+        </LayoutPage>
+
+    )
+}
+
+export default Settings
