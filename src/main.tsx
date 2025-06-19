57cc3563f6616550a62d00128d2e917aa6c38fc3
Nik91K <nikotakovsl@gmail.com>
2025-06-19 22:40:07 +0300

    add some styles and add statistics page

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,17 @@
+import './index.css'
+import App from './App.tsx'
 import { createRoot } from 'react-dom/client'
 import { BrowserRouter, Link, Route, Routes } from 'react-router-dom'
 import RegisterPage from './pages/Authentication/RegisterPage/RegisterPage'
 import LoginPage from './pages/Authentication/LoginPage/loginPage.tsx'
 import MainPage from './pages/SleepTracker/Main/MainPage.tsx'
+import SleepTrackerStatistics from './pages/SleepTracker/Statistics/SleepTrackerStatistics.tsx'
 import Settings from './pages/SleepTracker/Settings/Settings.tsx'
 import PrivateRoute from './pages/Authentication/PrivateRoute/PrivateRoute.tsx'
 import { Provider } from 'react-redux'
 import { store } from './api/store.ts'
 import Sitebar from './components/Layout/Sitebar/Sitebar.tsx'
 
-import './index.css'
-import App from './App.tsx'
 
 createRoot(document.getElementById('root')!).render(
   <Provider store={store}>
@@ -27,6 +28,7 @@ createRoot(document.getElementById('root')!).render(
         }>
           <Route index element={<MainPage />} />
           <Route path='settings' element={<Settings />} />
+          <Route path='statistics' element={<SleepTrackerStatistics />}/>
         </Route>
       </Routes>
     </BrowserRouter>
