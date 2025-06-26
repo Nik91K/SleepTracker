607c78567953d35735fd1ff798dc4e866b842b9f
Nik91K <nikotakovsl@gmail.com>
2025-06-26 18:22:49 +0300

    created user slice with theme and avatar

---
+++ src/api/slices/userSlice.ts
@@ -0,0 +1,89 @@
+import { createAction, createAsyncThunk, createSlice } from "@reduxjs/toolkit";
+import axios from '../axios'
+import type { UserConfigurationType } from "../../types/userConfigurationType";
+
+interface TypeState {
+    theme: string | null;
+    image: string | null;
+    loading: boolean;
+    error: string | null;
+    token: string | null;
+}
+
+const initialState: TypeState = {
+    theme: null,
+    image: null,
+    loading: false,
+    error: null,
+    token: localStorage.getItem('token') || null,
+}
+
+const SLICE_URL = "user"
+
+export const updateUserTheme = createAsyncThunk (
+  'user/updateTheme',
+  async (theme: string, { rejectWithValue }) => {
+    try {
+      const response = await axios.patch('/user/theme', { theme })
+      return response.data
+    } catch (error: any) {
+      return rejectWithValue(error.response.data)
+    }
+  }
+)
+
+export const updateUserAvatar = createAsyncThunk(
+    'user/updateAvatar',
+    async (file: File, { rejectWithValue }) => {
+        try {
+            const formData = new FormData()
+            formData.append('file', file)
+
+            const response = await axios.patch('/user/avatar', formData, {
+                // headers: {
+                //     'Content-Type': 'multipart/form-data',
+                // },
+            })
+
+            return response.data
+        } catch (error: any) {
+            return rejectWithValue(error.response.data)
+        }
+    }
+)
+
+const configurationSlice = createSlice ({
+    name: "configuration",
+    initialState,
+    reducers: {},
+    extraReducers: (builder) => {
+        builder
+        .addCase(updateUserTheme.pending, (state) => {
+            state.loading = true
+            state.error = null
+        })
+        .addCase(updateUserTheme.fulfilled, (state, action) => {
+            state.theme = action.payload.theme;
+            state.loading = false
+        })
+        .addCase(updateUserTheme.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.payload as string;
+        })
+
+        .addCase(updateUserAvatar.pending, (state) => {
+            state.loading = true
+            state.error = null
+        })
+        .addCase(updateUserAvatar.fulfilled, (state, action) => {
+            state.image = action.payload.image
+            state.loading = false
+        })
+        .addCase(updateUserAvatar.rejected, (state, action) => {
+            state.loading = false
+            state.error = action.payload as string
+        })
+    }
+})
+
+export default configurationSlice.reducer
