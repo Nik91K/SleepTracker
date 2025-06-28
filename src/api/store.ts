962295088781837f3616e59003d357edc91e19ea
Nik91K <nikotakovsl@gmail.com>
2025-06-28 09:34:20 +0300

    add configuration slice

--- src/api/store.ts
+++ src/api/store.ts
@@ -2,12 +2,14 @@ import { configureStore } from '@reduxjs/toolkit';
 import authSlice from './slices/authSlice'
 import sleepRecordSlice from './slices/SleepRecord';
 import sleepEfficiencySlice from './slices/sleepEfficiencySlice';
+import configurationSlice from './slices/userSlice'
 
 export const store = configureStore({
     reducer: {
         auth: authSlice,
         sleepRecord: sleepRecordSlice,
         sleepEfficiency: sleepEfficiencySlice,
+        users: configurationSlice
     },
 })
 
