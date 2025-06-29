17574eb796b1f90d4a7efb05a4b1b5df711c0853
Nik91K <nikotakovsl@gmail.com>
2025-06-29 18:44:23 +0300

    added notification slice

--- src/api/store.ts
+++ src/api/store.ts
@@ -3,13 +3,15 @@ import authSlice from './slices/authSlice'
 import sleepRecordSlice from './slices/SleepRecord';
 import sleepEfficiencySlice from './slices/sleepEfficiencySlice';
 import configurationSlice from './slices/userSlice'
+import notificationSlice from './slices/notificationSettings'
 
 export const store = configureStore({
     reducer: {
         auth: authSlice,
         sleepRecord: sleepRecordSlice,
         sleepEfficiency: sleepEfficiencySlice,
-        users: configurationSlice
+        users: configurationSlice,
+        notification:notificationSlice
     },
 })
 
