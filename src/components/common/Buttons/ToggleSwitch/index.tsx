4058e7641aefa0cfcbe6d77bdea274b54c69a9e4
Nik91K <nikotakovsl@gmail.com>
2025-06-29 23:20:38 +0300

    add toggle switch

---
+++ src/components/common/Buttons/ToggleSwitch/index.tsx
@@ -0,0 +1,13 @@
+import './style.css'
+
+const ToggleSwitch = () => {
+    return (
+        <>
+        <input type="checkbox" id="checkboxInput" />
+        <label htmlFor="checkboxInput" className="toggleSwitch">
+        </label>
+        </>
+    )
+}
+
+export default ToggleSwitch
