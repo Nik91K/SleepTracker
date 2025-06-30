c91e685e98b26b995dc8d9875eb592f47df7419d
Nik91K <nikotakovsl@gmail.com>
2025-06-30 23:23:05 +0300

    update reminder form

--- src/components/common/Forms/Reminder/index.tsx
+++ src/components/common/Forms/Reminder/index.tsx
@@ -4,19 +4,38 @@ import Input from '../../Input'
 import Tooltip from '../../Tooltip'
 import SubmitButton from '../../Buttons/SubmitButton'
 import React from 'react'
+import { updateNotificationSettings } from '../../../../api/slices/notificationSettings'
+import ToggleSwitch from '../../Buttons/ToggleSwitch/index'
 
 const ReminderForm = () => {
     const [tolltip, setError] = React.useState<{ type: 'success' | 'error'; message: string } | null>(null)
     const dispatch = useAppDispatch()
-    // const { loading, error } = useAppSelector(state => state.reminder)
+    const [enabled, setEnabled] = React.useState<boolean>(false)
+
+    const handleSubmit = async (event: React.FormEvent<HTMLFormElement>) => {
+        event.preventDefault()
+        let time = getFormInputValueByName(event.currentTarget, 'time')
+        let method: 'email' = 'email'
+
+        dispatch(updateNotificationSettings({ enabled, time, method }))
+        .unwrap()
+        .then(() => {
+            setError({type: 'success', message: 'Дані додано'})
+        }) .catch((error) => {
+            setError({ type: 'error', message: error })
+        })
+        event.currentTarget.reset()
+    }
+
     return (
         <>
         {tolltip && (
             <Tooltip type={tolltip.type} typeText={tolltip.message} close={() => setError(null)}/>
         )}
-        <form action="">
-            <Input type='text' title='Укажіть час нагадування'/>
-            <Input type='text' title='Текст для нагадування'/>
+        <form action="" onSubmit={handleSubmit}>
+            <ToggleSwitch checked={enabled} onChange={setEnabled}/>
+            <Input type='text' title='Укажіть час нагадування' name='time'/>
+            <SubmitButton type='submit' text='Надіслати' />
         </form>
         </>
     )
