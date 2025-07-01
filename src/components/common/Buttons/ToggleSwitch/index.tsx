bc7d8283bc4c4ac52aad44e47a673b0f6a972a04
Nik91K <nikotakovsl@gmail.com>
2025-07-01 13:21:14 +0300

    updated toggle switch

--- src/components/common/Buttons/ToggleSwitch/index.tsx
+++ src/components/common/Buttons/ToggleSwitch/index.tsx
@@ -1,13 +1,17 @@
+import Switch from '@mui/material/Switch';
 import './style.css'
 
-const ToggleSwitch = ({ checked, onChange } : { checked:boolean, onChange: (checked: boolean) => void }) => {
-    return (
-        <div>
-            <input type="checkbox" id="checkboxInput" checked={checked} onChange={(e) => onChange(e.target.checked)}/>
-            <label htmlFor="checkboxInput" className="toggleSwitch">
-            </label>
-        </div>
-    )
+const ToggleSwitch = ({ checked, onChange }: { checked: boolean; onChange: (checked: boolean) => void }) => {
+  return (
+    <div className='toggle-container'>
+        <p className='toggle-text'>Увімкнути нагадування</p>
+        <Switch
+            checked={checked}
+            onChange={(e) => onChange(e.target.checked)}
+            className='toggle-switch'
+        />
+    </div>
+  )
 }
 
 export default ToggleSwitch
