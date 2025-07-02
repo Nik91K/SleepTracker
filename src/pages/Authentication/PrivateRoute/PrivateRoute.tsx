c42a0b2c82b5a747107ee385f4e1c941dac45846
Nik91K <nikotakovsl@gmail.com>
2025-07-02 22:33:10 +0300

    children: ReactNode

--- src/pages/Authentication/PrivateRoute/PrivateRoute.tsx
+++ src/pages/Authentication/PrivateRoute/PrivateRoute.tsx
@@ -1,14 +1,14 @@
-import type { JSX } from "react";
+import type { ReactNode } from "react";
 import { Navigate } from "react-router";
 
-const PrivateRoute = ({ children }: { children: JSX.Element }) => {
+const PrivateRoute = ({ children }: { children: ReactNode }) => {
     const token = localStorage.getItem("token");
   
     if (!token) {
       return <Navigate to="/login" replace />
     }
   
-    return children;
+    return <>{children}</>;
   }
 
 export default PrivateRoute
