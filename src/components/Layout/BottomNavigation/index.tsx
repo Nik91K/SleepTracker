7be1c95eb7541152ac3d0c1adadceda3b2d5631d
Nik91K <nikotakovsl@gmail.com>
2025-07-03 22:39:40 +0300

    added active button

--- src/components/Layout/BottomNavigation/index.tsx
+++ src/components/Layout/BottomNavigation/index.tsx
@@ -1,21 +1,22 @@
 import './style.css'
 import { FaHome, FaCog } from 'react-icons/fa'
 import { FaChartSimple } from "react-icons/fa6";
-import { Link } from 'react-router'
+import { Link, useLocation } from 'react-router'
 
 const BottomNavigation = () => {
-
+    const currentPath = useLocation().pathname
+    
     return (
         <div className="bottom-nav">
-            <Link to="/sleeptracker" className="nav-btn">
+            <Link to="/sleeptracker" className={`navigation-button ${currentPath === '/sleeptracker' ? 'active' : ''}`}>
                 <FaHome />
                 <span>Home</span>
             </Link>
-            <Link to="/sleeptracker/statistics" className="nav-btn">
+            <Link to="/sleeptracker/statistics" className={`navigation-button ${currentPath === '/sleeptracker/statistics' ? 'active' : ''}`}>
                 <FaChartSimple />
                 <span>Statistics</span>
             </Link>
-            <Link to="/sleeptracker/settings" className="nav-btn">
+            <Link to="/sleeptracker/settings" className={`navigation-button ${currentPath === '/sleeptracker/settings' ? 'active' : ''}`}>
                 <FaCog />
                 <span>Settings</span>
             </Link>
