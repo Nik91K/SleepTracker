bbcc81f5424ff63555b9592409c50fd408494474
Nik91K <nikotakovsl@gmail.com>
2025-07-07 18:58:18 +0300

    updated axios

--- src/api/axios.ts
+++ src/api/axios.ts
@@ -1,15 +1,31 @@
 import axios from "axios"
 
 const instance = axios.create({
-    baseURL: 'http://localhost:3001/api/v1',
+    baseURL: `${import.meta.env.VITE_API_URL}/api/v1`,
+    withCredentials: true,
 })
 
-instance.interceptors.request.use((config: any) => {
-    const token:string | null = localStorage.getItem('token')
-    if (token) {
-        config.headers.Authorization = `Bearer ${token}`
+instance.interceptors.response.use(
+    (response) => response,
+    async (error) => {
+        const originalRequest = error.config
+        if (error.response?.status === 401 && !originalRequest._retry) {
+            originalRequest._retry = true
+            try {
+                const response = await axios.post(`${import.meta.env.VITE_API_URL}api/v1/auth/refresh`,
+                    {},
+                    { withCredentials: true }
+                )
+                const newToken = response.data.accessToken
+                instance.defaults.headers.common['Authorization'] = `Bearer ${newToken}`;
+                originalRequest.headers['Authorization'] = `Bearer ${newToken}`;
+                return instance(originalRequest)
+            } catch (error) {
+                console.log('Refresh error', error)
+            }
+        }
+        return Promise.reject(error)
     }
-    return config
-})
+)
 
 export default instance
