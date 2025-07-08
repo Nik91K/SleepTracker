889e8a0f18d71706de9d9d27f7ce9bbc1dcafe86
Nik91K <nikotakovsl@gmail.com>
2025-07-08 23:39:01 +0300

    updated main page

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,15 @@
 import LayoutPage from './layoutPage/layoutPage'
-
+import TopBlock from './components/Layout/MainPage/TopBlock/index'
+import MainImage from './assets/main-page-images/original-56917166d4465051909320a17201bec.webp'
 import './App.css'
 
 function App() {
 
   return (
     <LayoutPage title="Головна сторінка">
-      <div className="app">
-        <p>SleepTrack — це сучасний трекер сну, створений, щоб допомогти вам краще спати, більше відпочивати та почуватися енергійніше щодня.</p>
+      <div className="main-page-top-block">
+        <img src={MainImage} alt="" className='main-page-img'/>
+        <TopBlock title='Sleep Tracker App' subtitle='Wake up fresh' description='Сучасний трекер сну, створений, щоб допомогти вам краще спати'/>
       </div>
     </LayoutPage>
   )
