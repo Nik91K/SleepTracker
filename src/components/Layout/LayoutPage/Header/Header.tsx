29afbc600a8fa4b085436fdb22b48b02d1d2447b
Nik91K <nikotakovsl@gmail.com>
2025-07-10 21:33:05 +0300

    updated header

--- src/components/Layout/LayoutPage/Header/Header.tsx
+++ src/components/Layout/LayoutPage/Header/Header.tsx
@@ -4,13 +4,16 @@ import { IoLogInOutline } from "react-icons/io5";
 import Greetings from "../../../common/Greetings";
 import { useLocation } from 'react-router-dom';
 import './Header.css'
+import { useSelector } from "react-redux";
+import type { RootState } from "../../../../api/store";
 
 
 const Header = ({ isLoggedIn, pageTitle } : { isLoggedIn: boolean, pageTitle?:string }) => {
+    const { user, loading } = useSelector((state: RootState) => state.auth)
     const location = useLocation()
     return (
         <header className="header">
-            { !isLoggedIn &&
+            { !user &&
                 <>
                 <div className="header-title">
                     <TfiAlarmClock size={35} />
@@ -32,7 +35,7 @@ const Header = ({ isLoggedIn, pageTitle } : { isLoggedIn: boolean, pageTitle?:st
                 </div>
                 </>
             }
-            { isLoggedIn &&
+            { user &&
                 <div className="page-title">
                     {location.pathname === '/sleeptracker' && <Greetings />}
                     {location.pathname !== '/sleeptracker' && pageTitle && (
